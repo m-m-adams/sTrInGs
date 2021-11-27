fe795e1367f51c29410e7e0705df023faad26121
m-m-adams <52469396+m-m-adams@users.noreply.github.com>
2021-11-27 15:48:11 -0500

    support for line numbers

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 use std::str;
 use std::str::FromStr;
 use structopt::StructOpt;
-enum Encodings {
+enum Encoding {
     UTF8,
     UTF16BE,
     UTF16LE,
@@ -12,14 +12,14 @@ enum Encodings {
 // any error type implementing Display is acceptable.
 type ParseError = &'static str;
 
-impl FromStr for Encodings {
+impl FromStr for Encoding {
     type Err = ParseError;
     fn from_str(day: &str) -> Result<Self, Self::Err> {
         match day {
-            "s" => Ok(Encodings::UTF8),
-            "S" => Ok(Encodings::UTF8),
-            "l" => Ok(Encodings::UTF16LE),
-            "b" => Ok(Encodings::UTF16BE),
+            "s" => Ok(Encoding::UTF8),
+            "S" => Ok(Encoding::UTF8),
+            "l" => Ok(Encoding::UTF16LE),
+            "b" => Ok(Encoding::UTF16BE),
             _ => Err("Not an encoding option"),
         }
     }
@@ -34,15 +34,26 @@ struct Cli {
 
     /// minimum length to search for
     #[structopt(short = "n", long = "--bytes", default_value = "4")]
-    n: usize,
+    length: usize,
+
+    /// Print the hex offset within the file before each string
+    #[structopt(short = "o")]
+    offset: bool,
 
     ///probability of swapping case
     #[structopt(short = "p", long = "--swap-prob", alias = "-", default_value = "0.5")]
-    p: f32,
+    prob: f32,
     ///Select character size and endianness:
     ///s = 7-bit, S = 8-bit, {b,l} = 16-bit, {B,L} = 32-bit
     #[structopt(short = "e", long = "--encoding", possible_values=&["s", "S", "b", "l"], default_value="S")]
-    e: Encodings,
+    enc: Encoding,
+}
+
+struct Options {
+    length: usize,
+    prob: f32,
+    enc: Encoding,
+    off: bool,
 }
 fn is_ascii_printable(ch: char) -> bool {
     if ch.is_ascii_punctuation() || ch.is_ascii_alphanumeric() {
@@ -67,25 +78,30 @@ fn swap_case(c: char, prob: f32) -> u8 {
     return c as u8;
 }
 
-fn find_strings_ascii(f: Vec<u8>, len: usize, prob: f32) {
+fn find_strings_ascii(f: Vec<u8>, opt: &Options) {
     let mut string: Vec<u8> = Vec::new();
-    for c in f {
+    let mut line = 0;
+    for (count, c) in f.into_iter().enumerate() {
         if is_ascii_printable(c as char) {
-            let out = swap_case(c as char, prob);
+            let out = swap_case(c as char, opt.prob);
             string.push(out);
         } else {
-            if string.len() > len {
-                println!("{}", str::from_utf8(&string).unwrap());
+            if string.len() > opt.length {
+                match opt.off {
+                    true => println!("{:x}: {}", line, str::from_utf8(&string).unwrap()),
+                    _ => println!("{}", str::from_utf8(&string).unwrap()),
+                }
             }
             string.clear();
+            line = count;
         }
     }
 }
 
-fn find_strings_utf16(f: Vec<u8>, le: Encodings, len: usize, prob: f32) -> Result<(), ParseError> {
-    let frombytes = match le {
-        Encodings::UTF16BE => u16::from_ne_bytes,
-        Encodings::UTF16LE => u16::from_be_bytes,
+fn find_strings_utf16(f: Vec<u8>, options: &Options) -> Result<(), ParseError> {
+    let frombytes = match options.enc {
+        Encoding::UTF16BE => u16::from_ne_bytes,
+        Encoding::UTF16LE => u16::from_be_bytes,
         _ => return Err("not a valid encoding"),
     };
     let fu16: Vec<u16> = f
@@ -95,17 +111,22 @@ fn find_strings_utf16(f: Vec<u8>, le: Encodings, len: usize, prob: f32) -> Resul
         .collect();
 
     let mut string: Vec<u8> = Vec::new();
-    for c in char::decode_utf16(fu16) {
+    let mut line = 0;
+    for (count, c) in char::decode_utf16(fu16).enumerate() {
         match c {
             Ok(c) => {
                 if is_ascii_printable(c) {
-                    let out = swap_case(c, prob);
+                    let out = swap_case(c, options.prob);
                     string.push(out);
                 } else {
-                    if string.len() > len {
-                        println!("{}", str::from_utf8(&string).unwrap());
+                    if string.len() > options.length {
+                        match options.off {
+                            false => println!("{}", str::from_utf8(&string).unwrap()),
+                            true => println!("{:x}: {}", line, str::from_utf8(&string).unwrap()),
+                        }
                     }
                     string.clear();
+                    line = count * 2;
                 }
             }
             _ => {
@@ -117,16 +138,23 @@ fn find_strings_utf16(f: Vec<u8>, le: Encodings, len: usize, prob: f32) -> Resul
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let opt = Cli::from_args();
-    let f = fs::read(opt.in_path);
+    let cli = Cli::from_args();
+    let f = fs::read(cli.in_path);
+
+    let opt: Options = Options {
+        length: cli.length,
+        prob: cli.prob,
+        enc: cli.enc,
+        off: cli.offset,
+    };
 
     let f = match f {
         Ok(file) => file,
         Err(error) => panic!("Problem opening the file: {:?}", error),
     };
-    match opt.e {
-        Encodings::UTF8 => find_strings_ascii(f, opt.n, opt.p),
-        _ => find_strings_utf16(f, opt.e, opt.n, opt.p)?,
+    match opt.enc {
+        Encoding::UTF8 => find_strings_ascii(f, &opt),
+        _ => find_strings_utf16(f, &opt)?,
     }
 
     return Ok(());
